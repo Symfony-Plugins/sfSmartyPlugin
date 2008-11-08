e786cbd55d8078f940d009e9f0d6827f9e297c61
insaini <insaini@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-11-08 15:37:26 -0600

    Updated AppUrlHelper.php
    git-svn-id: http://svn.symfony-project.com/plugins/sfSmartyPlugin/trunk@12786 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/helper/AppUrlHelper.php
+++ lib/helper/AppUrlHelper.php
@@ -10,9 +10,9 @@ use_helper('Url');
 * @return string XHTML compliant <a href> tag
 */
 function link_to_app($name = '', $internal_uri = '', $options = array(), $app = SF_APP) {
-	$environment = (SF_ENVIRONMENT == 'prod') ? "" : "_".SF_ENVIRONMENT;
+	$environment = (sfConfig::get('sf_environment') == 'prod') ? "" : "_".sfConfig::get('sf_environment');
 	$protocol = (sfContext::getInstance()->getRequest()->isSecure()) ? "https" : "http";
-	$url = $protocol."://".$_SERVER["SERVER_NAME"].url_for("").strtolower($app).$environment. ".php/".$internal_uri;
+	$url = $protocol."://".$_SERVER["SERVER_NAME"]."/".strtolower($app).$environment. ".php/".$internal_uri;
 	return link_to($name, $url, $options);
 }
 ?>
